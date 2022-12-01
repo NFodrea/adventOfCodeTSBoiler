ee834815c88654b350b75916e8f940d52e86521c
Nathaniel <nathaniel.fodrea@genesys.com>
2022-12-01 17:48:56 -0500

    added a split and read file util

---
+++ src/utils/split.ts
@@ -0,0 +1,16 @@
+import fs from "fs-extra";
+
+export async function readFile(filePath: string) {
+    return await fs.readFile(filePath, { encoding: "utf8" });
+}
+
+export async function splitData(filePath: string) {
+    try {
+        const data = await readFile(filePath);
+        data.split("\n\n").map(x => x.split("\n"));
+        data.trim();
+        console.log(data);
+    } catch (err) {
+        console.log(err);
+    }
+}
